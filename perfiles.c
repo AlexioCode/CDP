26b9507c1764bcdd84bde5c128afd595213722ce
AlexioPlay <alexalonsobasurtodiaz@gmail.com>
2022-03-04 20:28:03 +0100

    Initial commit

---
+++ perfiles.c
@@ -0,0 +1,10 @@
+
+perfil_profesor(char * profesor);
+void elegir_grupo_materia();
+void menu_profe_grupo_materia()
+{
+    listaalum()
+}
+listalum()
+{
+}
---
+++ perfiles.h
@@ -0,0 +1,18 @@
+#ifndef PERFILES_H_INCLUDED
+#define PERFILES_H_INCLUDED
+
+typedef struct
+{
+    float Id_alum;
+    char Nombre_alum [21];
+
+    char
+}alumno;
+
+
+//cabecera: perfil_profesor()
+//
+//
+perfil_profesor(char * profesor);
+
+#endif // PERFILES_H_INCLUDED
