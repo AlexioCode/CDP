91d31b07cf7c8bf8f1330397ddb532f41b0c08d4
martaajonees <100365874+martaajonees@users.noreply.github.com>
2022-03-25 13:53:18 +0100

    Main

---
+++ main PPal.c
@@ -0,0 +1,16 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+//Incluimos archivos.h
+#include"horarios.h"
+#include"alumnos.h"
+#include"materia.h"
+#include"calificaciones.h"
+#include"usuarios.h"
+//Tamaño vectores
+int N=nLineas("calificaciones.txt");
+int A=nLineas("alumnos.txt");
+int Y=nLineas("materia.txt");
+int M=nLineas("usuarios.txt");
+int B=nLineas("horarios.txt");
