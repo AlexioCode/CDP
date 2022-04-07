f851cd25078e840c37535c9ed0591eea0861b95f
Daniel Jesús Fernández <101361963+AstroNerd43@users.noreply.github.com>
2022-04-07 21:17:34 +0200

    Add files via upload

--- Guardar_Horarios.c
+++ Guardar_Horarios.c
@@ -1,17 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-int tam=6;
-typedef struct{
-int ID_prof;
-int dia_clase;
-int hora_clase;
-int ID_materia;
-char grupo[10];
-}horari;
-horari horarios[tam];
-
-void Guardar_calificaciones(horari **horarios, int *tam){
+void Guardar_Horarios(horari **horarios, int *tam){
 int cont;
 FILE *f;
 f=fopen("Horarios.txt","w");
@@ -19,7 +9,8 @@ if (f==NULL){ //Comprobación de si el fichero se ha abierto.
     printf("Horarios.txt no pudo abrirse.\n");
     exit(-1);
 }
-for (cont=0;cont<*tam-1;cont++){
+int x=*tam;
+for (cont=0;cont<x-1;cont++){
     fprintf(f,"%s-%s-%s-%s-%s\n",(*horarios)[cont].ID_prof,(*horarios)[cont].dia_clase,(*horarios)[cont].hora_clase,(*horarios)[cont].ID_materia,(*horarios)[cont].grupo);
 }
 fprintf(f,"%s-%s-%s-%s-%s\n",(*horarios)[cont].ID_prof,(*horarios)[cont].dia_clase,(*horarios)[cont].hora_clase,(*horarios)[cont].ID_materia,(*horarios)[cont].grupo);
--- Guardar_calificaciones.c
+++ Guardar_calificaciones.c
@@ -1,21 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-int tam=6;
-typedef struct{
-int dia;
-int mes;
-int anno;
-}fec;
-
-typedef struct{
-fec fecha;
-char descrip[30];
-int materia;
-int alum;
-int not;
-}calificaciones;
-calificaciones nota[tam];
 
 void Guardar_calificaciones(calificaciones **nota, int *tam){
 int cont;
@@ -25,9 +10,10 @@ if (f==NULL){ //Comprobación de si el fichero se ha abierto.
     printf("Calificaciones.txt no pudo abrirse.\n");
     exit(-1);
 }
-for (cont=0;cont<*tam-1;cont++){
+int x=*tam;
+for (cont=0;cont<x-1;cont++){
     fprintf(f,"%s-%s-%s-%s-%s-%s-%s\n",(*nota)[cont].fecha.dia ,(*nota)[cont].fecha.mes, (*nota)[cont].fecha.anno,(*nota)[cont].descrip,(*nota)[cont].materia,(*nota)[cont].alum,(*nota)[cont].not);
 }
-fprintf(f,"%s-%s-%s-%s-%s-%s-%s",(*nota)[cont].fecha.dia ,(*nota)[cont].fecha.mes, (*nota)[cont].fecha.anno,(*nota)[cont].descrip,(*nota)[cont].materia,(*nota)[cont].alum,(*nota)[cont].not);
+fprintf(f,"%s-%s-%s-%s-%s-%s-%s\n",(*nota)[cont].fecha.dia ,(*nota)[cont].fecha.mes, (*nota)[cont].fecha.anno,(*nota)[cont].descrip,(*nota)[cont].materia,(*nota)[cont].alum,(*nota)[cont].not);
 fclose(f); //Cierre del fichero.
 }
