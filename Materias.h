ec9fef74adade413bf3fba49dd21911ad3a85875
Javier <ja.cabegar@alum.uca.es>
2022-04-07 17:36:13 +0200

    Materias.c y materias.h
    Cambios a tipos de funciones, variables para los finales de bucle.
    Cambios a aplicar también en matriculas.

--- Materias.h
+++ Materias.h
@@ -9,24 +9,24 @@ typedef struct{
 }materia;
 
 int tam_materia();
-void cargar_materias();
+materia * cargar_materias();
 void guardar_materias(materia *p, int tam_max);
 
 
-void cambiar_id_mat(materia *p, char* id, char* nuevo_id);
+void cambiar_id_mat(materia *p, int tam_max);
 
-void cambiar_nombre_mat(materia *p, char* nombre, char* nuevo_nombre);
+void cambiar_nombre_mat(materia *p, int tam_max);
 
-void cambiar_siglas_mat(materia *p, char* siglas, char* nuevo_siglas);
+void cambiar_siglas_mat(materia *p, int tam_max);
 
 // tam_max debe de ser un valor devuelto por cargar_materias() o alta()
 void ver_materias(materia *p, int tam_max);
 
 
-void baja_materia(materia *p, char* id);
+void baja_materia(materia *p, char* id, int * tam_max);
 
 // tam_max debe de ser un valor devuelto por cargar_materias o alta()
-int alta_materia(materia *p, char* nuevo_id, char* nuevo_nombre, char* nuevo_siglas, int tam_max);
+int alta_materia(materia **p, char* nuevo_id, char* nuevo_nombre, char* nuevo_siglas, int tam_max);
 
 
 
