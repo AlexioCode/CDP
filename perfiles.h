e3b857b62445686174618208f1dd0a89958f2d73
AlexioPlay <alexalonsobasurtodiaz@gmail.com>
2022-04-07 17:06:43 +0200

    Update perfiles.h

--- perfiles.h
+++ perfiles.h
@@ -1,10 +1,15 @@
 #ifndef PERFILES_H_INCLUDED
 #define PERFILES_H_INCLUDED
+#include "alumnos.h"
+#include "calificaciones.h"
+#include "horarios.h"
+#include "Materias.h"
+#include "Matriculas.h"
 #include "usuarios.h"
 
 //char * login(usuario ** pv_usuarios)
 //precondicion: recibe punteros a vectores inicializados
-//postcondicion: devuelve el Id_usuario del usuario con ese usuario y contraseña, y llama al perfil correspondiente (de profesor o de administrador)
+//postcondicion: devuelve el Id_usuario del usuario con ese usuario y contraseÃ±a, y llama al perfil correspondiente (de profesor o de administrador)
 char * login(alum ** alumnos, calificaciones ** v_calif, horari ** v_fechas, materia ** materias, matricula ** v_matriculas, usuario ** pv_usuarios);
 
 #endif // PERFILES_H_INCLUDED
