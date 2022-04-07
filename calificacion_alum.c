d6648394e16980ab7530657d3d0b6dfddda769fd
martaajonees <100365874+martaajonees@users.noreply.github.com>
2022-04-07 13:01:54 +0200

    v_actualizada calificacion_alum

--- calificacion_alum.c
+++ calificacion_alum.c
@@ -20,11 +20,11 @@ int borrar(int alum, int materia){
 int i,j=0,k,u=0;
 for(i=0;i<N;i++){
     if(nota[i].alum==alum && nota[i].materia==materia){
-        printf("El dia %i del mes %i en el anno %i saco un %i en el %c\n",nota[i].fecha.dia, nota[i].fecha.mes, nota[i].fecha.anno, nota[i].not, nota[i].descrip);
+        printf("El dia %i del mes %i en el anno %i saco un %i en el %s\n",nota[i].fecha.dia, nota[i].fecha.mes, nota[i].fecha.anno, nota[i].not, nota[i].descrip);
         do{
-        printf("¿Desea eliminar la nota? Introduzca 1 para sí, 0 para no\n");
-        scanf("%i",j);
-        }while(j!=1 || j!=0);
+        printf("ÀDesea eliminar la nota? Introduzca 1 para Si, 0 para No\n");
+        scanf("%d",&j);
+        }while(j>1 || j<0);
         if(j==1){
             for(k=i;k<(N-1);k++){
                 nota[i]=nota[i+1];
@@ -40,7 +40,7 @@ void ver_nota(int alum, int materia){
 int i,j=0;
 for(i=0;i<N;i++){
     if(nota[i].alum==alum && nota[i].materia==materia){
-        printf("El dia %i del mes %i en el anno %i saco un %i en el %c\n",nota[i].fecha.dia, nota[i].fecha.mes, nota[i].fecha.anno, nota[i].not, nota[i].descrip);
+        printf("El dia %i del mes %i en el anno %i saco un %i en el %s\n",nota[i].fecha.dia, nota[i].fecha.mes, nota[i].fecha.anno, nota[i].not, nota[i].descrip);
         j++;
         }
     }
@@ -53,18 +53,18 @@ void mod_nota(int alum, int materia){
 int i,j=0,k=-1,p=0;
 for(i=0;i<N;i++){
     if(nota[i].alum==alum && nota[i].materia==materia){
-        printf("El dia %i del mes %i en el anno %i saco un %i en el %c\n",nota[i].fecha.dia, nota[i].fecha.mes, nota[i].fecha.anno, nota[i].not, nota[i].descrip);
+        printf("El dia %i del mes %i en el anno %i saco un %i en el %s\n",nota[i].fecha.dia, nota[i].fecha.mes, nota[i].fecha.anno, nota[i].not, nota[i].descrip);
         p++;
         do{
         printf("¿Desea modificar la nota? Introduzca 1 para sí, 0 para no\n");
-        scanf("%i",j);
+        scanf("%d",&j);
         }while(j==1 || j==0);
         if(j==1){
             do{
             printf("Introduzca una nota valida entre 0-10\n");
-            scanf("%i",k);
+            scanf("%d",&k);
             }while(k>=0 && k<11);
-            nota[i].not==k;
+            nota[i].not=k;
             }
         }
     }
@@ -82,19 +82,19 @@ if(i>0){
 }
 
 void anadir_not(int alum, int materia){
-char descrip[30];
+
 int a=1,b=1,c=2022;
 printf("Indica el anno en el que quiere poner la nota\n");
-scanf("%i",c);
-nota[N-1].fecha.anno==c;
+scanf("%d",&c);
+nota[N-1].fecha.anno=c;
 do{
 printf("Indica el mes en el que quiere poner la nota\n");
-scanf("%i",b);
+scanf("%d",&b);
 }while(b<13 && b>0);
-nota[N-1].fecha.mes==b;
+nota[N-1].fecha.mes=b;
 do{
 printf("Indica el dia en el que quiere poner la nota\n");
-scanf("%i",a);
+scanf("%d",&a);
 if(b==2 && a==29 && c%4!=0){
     a=32;
 }
@@ -104,7 +104,7 @@ nota[N-1].alum=alum;
 nota[N-1].materia=materia;
 do{
     printf("Introduzca una nota entre 0 y 10 \n");
-    scanf("%i",a);
+    scanf("%d",&a);
 }while(a<0 || a>10);
 nota[N-1].not=a;
 printf("Introduzca una descripcion del examen\n");
@@ -127,13 +127,13 @@ do{
     printf("###################################################################################\n");
     printf("Bienvenido al menu de calificaciones \n");
     do{
-        printf("¿Que desea hacer? \n Pulse el numero correspondiente a la accion que desee hacer \n");
+        printf("ÀQue desea hacer? \n Pulse el numero correspondiente a la accion que desee hacer \n");
         printf("1.Listar las notas \n");
         printf("2.Modificar las notas \n");
         printf("3.Eliminar una nota \n");
         printf("4.Aniadir una nota \n");
         printf("5.Salir de este menu \n");
-        scanf("%i",a);
+        scanf("%d",&a);
     }while(a<6 && a>0);
     switch(a){
         case 1: ver_nota(alum,materia);
@@ -147,8 +147,8 @@ do{
         case 5: return;
                 break;
             }
-    printf("¿Desea hacer algo mas en este menu? \n Pulse 1 para sí, y 0 para no \n");
-    scanf("%i",b);
+    printf("ÀDesea hacer algo mas en este menu? \n Pulse 1 para Si, y 0 para No \n");
+    scanf("%d",&b);
     }while(b==1);
 printf("#####################################################################################################\n");
 }
