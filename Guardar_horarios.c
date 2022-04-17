07975226e46a2b656ca698f9b352fa1abcc36cf3
AlexioPlay <alexalonsobasurtodiaz@gmail.com>
2022-04-17 18:37:13 +0200

    update

--- Guardar_horarios.c
+++ Guardar_horarios.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-void Guardar_Horarios(horari *horarios){
+void Guardar_Horarios(horari **horarios){
 int cont;
 FILE *f;
 f=fopen("Horarios.txt","w");
@@ -11,8 +11,8 @@ if (f==NULL){ //Comprobaci�n de si el fichero se ha abierto.
     exit(-1);
 }
 for (cont=0;cont<tam_horari-1;cont++){
-    fprintf(f,"%d-%d-%d-%d-%s\n",horarios[cont].ID_prof ,horarios[cont].dia_clase, horarios[cont].hora_clase,horarios[cont].ID_materia,horarios[cont].grupo);
+    fprintf(f,"%d-%d-%d-%d-%s\n",(*horarios)[cont].ID_prof ,(*horarios)[cont].dia_clase, (*horarios)[cont].hora_clase,(*horarios)[cont].ID_materia,(*horarios)[cont].grupo);
 }
-    fprintf(f,"%d-%d-%d-%d-%s\n",horarios[cont].ID_prof ,horarios[cont].dia_clase, horarios[cont].hora_clase,horarios[cont].ID_materia,horarios[cont].grupo);
+    fprintf(f,"%d-%d-%d-%d-%s\n",(*horarios)[cont].ID_prof ,(*horarios)[cont].dia_clase, (*horarios)[cont].hora_clase,(*horarios)[cont].ID_materia,(*horarios)[cont].grupo);
 fclose(f); //Cierre del fichero.
 }
