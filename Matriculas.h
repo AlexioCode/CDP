be7d05470d07bce2c75f1e209874bbea193bae9e
Dreivax2 <62073171+Dreivax2@users.noreply.github.com>
2022-04-17 12:30:14 +0200

    Update Matriculas.h

--- Matriculas.h
+++ Matriculas.h
@@ -2,6 +2,7 @@
 #ifndef MATRICULAS_H_INCLUDED
 #define MATRICULAS_H_INCLUDED
 
+int tam_matric;
 typedef struct{
     char matid[5];
     char alumid[7];
@@ -14,11 +15,11 @@ int tam_matricula();
 //char* cargar_matriculas();
 //Tener el ficher matriculas.txt
 //Devuelve el puntero al primer elemento del vector matriculas
-char* cargar_matriculas();
-//void guardar_matriculas(matricula *p, int tam_max);
+matricula* cargar_matriculas();
+//void guardar_matriculas(matricula *p, int tam_matric);
 //Tener el fichero matriculas.txt, puntero al vector de matriculas y tener el tamaño máximo (tam_matricula/alta_matricula)
 //
-void guardar_matriculas(matricula *p, int tam_max);
+void guardar_matriculas(matricula *p);
 //void cambiar_id_matriculas(matricula *p);
 //Puntero al vector de matricula
 //
@@ -27,17 +28,17 @@ void cambiar_id_matriculas(matricula *p);
 //Puntero al vector de matricula
 //
 void cambiar_alumid_matriculas(matricula *p);
-//void ver_matriculas(matricula *p, int tam_max);
+//void ver_matriculas(matricula *p, int tam_matric);
 //Puntero al vector de matricula y tener el tamaño máximo (tam_matricula/alta_matricula)
 //
-void ver_matriculas(matricula *p, int tam_max);
+void ver_matriculas(matricula *p, int tam_matric);
 //void baja_matricula(matricula *p, char *matid);
 //Puntero al vector de matricula e id de la matricula de baja
 //
-int baja_matricula(matricula *p, char *matid, int tam_max);
-//int alta_matricula(matricula *p, char *matid, char *alumid, int tam_max);
+void baja_matricula(matricula *p, char *matid);
+//int alta_matricula(matricula *p, char *matid, char *alumid, int tam_matric);
 //id e id del alumno de la matricula, puntero al vector de matricula y tener el tamaño máximo (tam_matricula/alta_matricula)
 //Devuelve el tamaño aumentado en uno
-int alta_matricula(matricula *p, char *matid, char *alumid, int tam_max);
+void alta_matricula(matricula *p, char *matid, char *alumid);
 
 #endif // MATRICULAS_H_INCLUDED
