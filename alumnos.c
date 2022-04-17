0ee2afdfc1ffc3c1aaa7c8a0f973884c2c76f565
martaajonees <100365874+martaajonees@users.noreply.github.com>
2022-04-17 11:45:24 +0200

    Update alumnos.c

--- alumnos.c
+++ alumnos.c
@@ -83,7 +83,8 @@ fclose(f); //Cierre del fichero.
 //Postcondición: Da de alta al alumno seleccionado.
 void alta(r_alum* alum){
     char res[3];
-    alum= (r_alum*) malloc(sizeof(alum)*(tam_alum+1));//Aumentamos la reserva +1
+    tam_alum=tam_alum+1;
+    alum= (r_alum*) malloc(sizeof(r_alum)*(tam_alum+1));//Aumentamos la reserva +1
     printf("Introduzca los datos siguientes del alumno que quiere añadir:");
     printf("Id: ");
     scanf("%s",alum[tam_alum+1].Id_alum);
